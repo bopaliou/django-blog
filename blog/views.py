863717df7efaa10a694899d772fdaf70651c2f73
Aliou Bop <bopaliou1994@gmail.com>
2025-08-07 17:06:08 +0000

    add display and detail articles views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+
+def home(request):
+    articles = Article.objects.filter(est_publie=True).order_by('-date_publication')
+    return render(request, 'blog/home.html', {'articles': articles})
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk, est_publie=True)
+    return render(request, 'blog/article_detail.html', {'article': article})
